9659ab555f454ea39cf1369a696b3c1c5283f1fe
harunarukayya567-a11y <harunarukayya567@gmail.com>
2025-12-23 07:15:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-delete this file
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Fraud Detection Project is running successfully"
+
+if __name__ == "__main__":
+    app.run()
+
